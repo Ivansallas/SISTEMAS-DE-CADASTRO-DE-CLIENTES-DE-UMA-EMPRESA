07016bf8bc117fda9c007f4a57a8b27b2b3eca6f
Júlia F. Argueles <julia.argueles@escola.pr.gov.br>
2025-06-16 08:28:07 -0300

    cabeçalho

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
-int main(){
-    printf("teste uploading");
-    printf("Ola deu tudo certo com update");
-    printf("TESTE FORK");
-}
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
+#define MAX_CLIENTES 1000
 
+typedef struct {
+    char nome[100];
+    char cpf[15];
+    char email[100];
+    char telefone[20];
+} Cliente;
+
+void carregarClientes(Cliente clientes[], int *total);
+void salvarClientes(Cliente clientes[], int total);
+void cadastrarCliente(Cliente clientes[], int *total);
+void listarClientes(Cliente clientes[], int total);
+void buscarCliente(Cliente clientes[], int total);
+void editarCliente(Cliente clientes[], int total);
+void removerCliente(Cliente clientes[], int *total);
+
+#endifm
